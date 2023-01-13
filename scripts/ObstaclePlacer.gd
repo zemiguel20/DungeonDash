f41e1be757325fe9d371cc5972e35d636700e86d
MrElectrodude <60736827+MrElectrodude@users.noreply.github.com>
2023-01-13 13:00:34 +0100

    Fixed sync

--- scripts/ObstaclePlacer.gd
+++ scripts/ObstaclePlacer.gd
@@ -21,6 +21,7 @@ var soundcue_types = [] # 0 = click, 1 = cut, 2 = grunt
 var next_loc
 var ctr = 0
 
+# List of obstacle locations
 var obstacle_locs = []
 
 # Scene and various obstacles that can be placed.
@@ -28,6 +29,10 @@ var scene
 var spikes
 var axe
 var enemy
+
+# Time related vars
+var time_begin
+var time_delay
 
 # Called when the node enters the scene tree for the first time.
 func _ready():	
@@ -45,10 +50,6 @@ func _ready():
 	total_distance = speed * 60
 	inv_total_distance = float(total_distance) / bpm
 	
-	print(speed)
-	
-	print(inv_total_distance)
-	
 	# Calculate all x coords that line up with a given bpm.
 	for i in range(bpm + 1):
 		potential_locs.append(i * inv_total_distance)
@@ -61,19 +62,24 @@ func _ready():
 			obstacle_locs.append(potential_locs[j + timing])
 			
 			place_obstacle(0, potential_locs[j + timing])
-		
-	print(soundcue_locs)
 	
 	# Set next_loc to the first soundcue location
 	next_loc = soundcue_locs[0]
 	
+	# Calculate time begin and delays, needed to compensate for audio playing delays.
+	time_begin = OS.get_ticks_usec()
+	time_delay = AudioServer.get_time_to_next_mix() + AudioServer.get_output_latency()
 	$Song.play()
 
 
 func _process(delta):
-	# Calculate the elapsed time and travelled distance.
-	time_elapsed += delta
-	distance = time_elapsed * speed
+	# Compensate for audio playing delays.
+	var time = (OS.get_ticks_usec() - time_begin) / 1000000.0
+	time -= time_delay + 0.1
+	time = max(0, time)
+	
+	# Calculate the travelled distance.
+	distance = time * speed
 	
 	# Play the correct sound if the player has reached a soundcue location.
 	if distance > next_loc && ctr < len(soundcue_locs) - 1:
@@ -85,7 +91,7 @@ func _process(delta):
 			2:
 				$SoundCueGrunt.play()
 
-
+		# Lookout for the next soundcue location
 		ctr += 1
 		next_loc = soundcue_locs[ctr]
 
@@ -94,6 +100,8 @@ func _process(delta):
 func place_obstacle(type, pos):
 	var node
 	
+	# TODO: change the positions to the final ground and place activation boxes
+	# (or something similar)
 	match (type):
 		0:
 			node = spikes.instance()
