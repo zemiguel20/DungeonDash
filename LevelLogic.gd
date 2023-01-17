6d8044908ebb68fc450593e51f8ec977e9691d12
zemiguel20 <jmmscoutinho@gmail.com>
2023-01-17 23:53:35 +0100

    Added restarting and quiting to game logic

--- LevelLogic.gd
+++ LevelLogic.gd
@@ -1,4 +1,21 @@
 extends Node
 
+signal level_finished
+signal player_died
+
 func _player_hit(player):
 	player.die()
+	emit_signal("player_died")
+
+func _finish_line_reached(_body):
+	print("LEVEL FINISHED")
+	$Player.set_physics_process(false)
+	emit_signal("level_finished")
+
+func restart():
+# warning-ignore:return_value_discarded
+	get_tree().reload_current_scene()
+
+func quit():
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://scenes/MainMenu.tscn")
